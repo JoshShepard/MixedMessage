63ae0709b0a1df110b79591d919351eb4ef97cd3
JoshShepard <joshua_shepard@yahoo.com>
2024-12-27 20:28:33 -0500

    Create to-do list, test environment, successful log to console

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("hello project");
+
+console.log('You should learn: ${hobby}');
+console.log('The next anime you should watch is ${anime}');
+console.log('Take a vacation to: ${location}');
